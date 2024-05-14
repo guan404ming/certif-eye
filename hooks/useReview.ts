2f4b7d3c92e44c9f5f1c89d920fe4e0a5f4456a1
GUAN MING <guanmingchiu@gmail.com>
2024-05-15 01:14:42 +0800

    fix: update url

--- hooks/useReview.ts
+++ hooks/useReview.ts
@@ -1,6 +1,10 @@
+const URL = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`
+  : "http://localhost:3000/api";
+
 export default function useReview() {
   async function getReviewPoint(review: string) {
-    const res = await fetch("http://localhost:3000/api/infer", {
+    const res = await fetch(`${URL}/infer`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
